e5be12667aae0895d1b110e56c00d1ed6829f84c
wagnerfelippe <wag3ipi@gmail.com>
2025-07-31 22:24:43 -0300

    Novo exercício com a API mais funcional

---
+++ python/ExerciciosPython5.1.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import requests
+
+url_brazil = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+url_countries = 'https://covid19-brazil-api.vercel.app/api/report/v1/countries'
+caminho = 'W:/Engenharia de Dados/Specialization in Apache Airflow/Data-Engineer/Data-Engineer-1/python/'
+
+response = requests.get(url_brazil)
+response2 = requests.get(url_countries)
+#print(response)
+
+result_br = response.json()
+dados_br = result_br['data']
+
+result_countries = response2.json()
+dados_countries = result_countries['data']
+
+df_br = pd.DataFrame(dados_br)
+df_countries = pd.DataFrame(dados_countries)
+
+del df_countries['cases']
+del df_countries['recovered']
+
+df_countries.columns = [
+    'pais',
+    'total_confirmados',
+    'total_obitos',
+    'atualização'
+]
+
+df_countries.drop(columns=['atualização'], inplace=True)
+
+df_countries.to_csv(caminho + 'cleaned_covid19.csv', index=False)
+print('Programa feito e arquivo baixo no caminho: ' , caminho)
+
+
+
